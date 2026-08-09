f0dd089f6786415466c969f562b843a2a91bf6b4
aymanahmed332004-ship-it <aymanahmed332004@gmail.com>
2026-08-10 02:04:25 +0300

    Update build.gradle.kts

--- frontend/mobile/android/app/build.gradle.kts
+++ frontend/mobile/android/app/build.gradle.kts
@@ -26,14 +26,14 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode()
-        versionName = flutter.versionName()
+        versionCode = flutterVersionCode
+        versionName = flutterVersionName
     }
 
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
+            // Signing with the debug keys for now, so `flutter build --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
